e72a436a6d3b6f5b68d8cd23562f8c0d8f67e84e
Anju1415 <anjuannu1415@gmail.com>
2021-05-23 14:31:49 +0530

    replace fun with object in library

--- ToasterLibrary/src/main/java/com/auth/toasterlibrary/ToasterMessage.kt
+++ ToasterLibrary/src/main/java/com/auth/toasterlibrary/ToasterMessage.kt
@@ -3,8 +3,7 @@ package com.auth.toasterlibrary
 import android.content.Context
 import android.widget.Toast
 
-
-class ToasterMessage {
+object ToasterMessage {
     fun s(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
--- app/src/main/java/com/auth/toasterexample/MainActivity.kt
+++ app/src/main/java/com/auth/toasterexample/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.auth.toasterexample
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.auth.toasterlibrary.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn: Button
+    lateinit var lay :LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.hello)
+       btn.setOnClickListener {
+       }
     }
 }
